118a54ead781f14d8251f8fdf47b1fda46dd2bd8
Kase <133991306+YoderLN@users.noreply.github.com>
2025-04-25 19:16:46 -0500

    Add files via upload
    Ported logging function and all related functions from driving class to OutputLogger class

--- Program4.java
+++ Program4.java
@@ -15,15 +15,12 @@
 //********************************************************************
 import java.util.Scanner;
 import java.util.ArrayList;
-import java.io.FileNotFoundException;
-import java.util.Formatter;
 import java.security.SecureRandom;
 
 public class Program4 {
-	
+	private OutputLogger outputLogger = new OutputLogger();//enclosed all logging operations to class
 	private final int MAX_LEVEL_SIZE = 7;
 	private ArrayList<Double> levelScores = new ArrayList<>();
-	private Formatter output = null;
 	private Scanner input = new Scanner(System.in);
 	private String [] correctAnswer = {"Excellent!",
 										"Very good!",
@@ -37,40 +34,34 @@ public class Program4 {
 										"No. Don't give up!",
 										"Incorrect. Keep trying!"};
 	
-	private SecureRandom randomResponse = new SecureRandom();
-
+	private SecureRandom randomResponse = new SecureRandom();	
+	//***************************************************************
+	//
+	//  Method:       main
+	// 
+	//  Description:  The main method of the program
+	//
+	//  Parameters:   A String Array
+	//
+	//  Returns:      N/A 
+	//
+	//**************************************************************
 	public static void main(String[] args)
 	{
 		Program4 obj = new Program4();
+		
 		obj.developerInfo();
-		try {
-		obj.openOutputFile();
-		obj.outputHeader();
-		obj.primaryLoop();
-		obj.displayAverages();
-		obj.closeOutputFile();	
-			}
-	catch (Exception e)
-	{
-		obj.toConsoleAndLog(e.toString());
-	}
-}
-	   //***************************************************************
-	   //
-	   //  Method:       toConsoleAndLog
-	   // 
-	   //  Description:  writes to both logfile and console
-	   //
-	   //  Parameters:   String to be written
-	   //
-	   //  Returns:      N/A 
-	   //
-	   //**************************************************************
-		public void toConsoleAndLog(String message)
+		try 
 		{
-		System.out.println(message);
-		output.format("%s%n", message);
+			obj.primaryLoop();
+			obj.displayAverages();		
 		}
+		catch (Exception e)
+		{
+			System.err.println(e.toString() + "in " + e.getStackTrace()[0].getMethodName());
+		}
+	}
+
 	   //***************************************************************
 	   //
 	   //  Method:       displayAverages
@@ -87,83 +78,14 @@ public class Program4 {
 		for (int i = 0; i < levelScores.size(); i++)
 			{
 			String result = String.format("Level %d score : %.2f Percent", i+1, levelScores.get(i) );
-			toConsoleAndLog(result);
-			}
-		
+			outputLogger.toConsoleAndLog(result);
+			}	
 			if (levelScores.get(0) < 80)
 			{
-				toConsoleAndLog("Please ask your teacher for extra help.");
+				outputLogger.toConsoleAndLog("Please ask your teacher for extra help.");
 			}
-		
+		outputLogger.closeOutput();
 		}
-	   //***************************************************************
-	   //
-	   //  Method:       toLog
-	   // 
-	   //  Description:  writes to log file
-	   //
-	   //  Parameters:   String to be written
-	   //
-	   //  Returns:      N/A 
-	   //
-	   //**************************************************************
-		public void toLog(String message)
-		{
-			output.format("%s%n", message);
-		}
-	   //***************************************************************
-	   //
-	   //  Method:       openOutputFile
-	   // 
-	   //  Description:  opens output file
-	   //
-	   //  Parameters:   None
-	   //
-	   //  Returns:      N/A 
-	   //
-	   //**************************************************************
-	   public void openOutputFile()
-	   {
-	   	   try 
-	   	   	{
-	   	   		output = new Formatter("Program4-output.txt");	
-	   	   	}
-	   	   	catch (FileNotFoundException e) 
-	   	   {
-	   	        System.err.println("Issue with creating output file.");
-	   	        System.err.println(e.toString());
-	   	   }	   
-	   } 	   
-	   //***************************************************************
-	   //
-	   //  Method:       closeOutputFile
-	   // 
-	   //  Description:  closes output file
-	   //
-	   //  Parameters:   None
-	   //
-	   //  Returns:      N/A 
-	   //
-	   //**************************************************************
-	   public void closeOutputFile()
-	   {
-		   output.close();
-	   }
-	   //***************************************************************
-	   //
-	   //  Method:       outputHeader
-	   // 
-	   //  Description:  identifies file purpose
-	   //
-	   //  Parameters:   None
-	   //
-	   //  Returns:      N/A 
-	   //
-	   //**************************************************************
-	   public void outputHeader()
-	   {
-		   toLog("Log File for Program 4");
-	   }
 	   //***************************************************************
 	   //
 	   //  Method:       isValidFormat
@@ -181,10 +103,8 @@ public class Program4 {
 	   public boolean isValidFormat(String answer)
 	   {
 		 //easiest way to handle is assume true, then flip to false if issue found
-		   boolean result = true; 
-		   
-		   char firstCh = answer.charAt(0);
-		   
+		   boolean result = true; 	   
+		   char firstCh = answer.charAt(0);	   
 		   //early exit check
 		   if (answer.length() < 1 //empty string
 				   || answer.length() > 6 //string too long
@@ -212,7 +132,7 @@ public class Program4 {
 	   //
 	   //  Method:       primaryLoop
 	   // 
-	   //  Description:  
+	   //  Description:  controlling function for individual levels
 	   //
 	   //  Parameters:   None
 	   //
@@ -221,6 +141,7 @@ public class Program4 {
 	   //**************************************************************
 	   public void primaryLoop()
 	   {
+		   outputLogger.outputHeader();
 		   String result1 = "";
 		   String result2 = "";
 		   String result3 = "";
@@ -229,35 +150,31 @@ public class Program4 {
 		   {
 			   if (result1.equals(""))
 			   {
-				   //call first level
-				   
-				   toConsoleAndLog("Basic Level");
+				   //call first level			   
+				   outputLogger.toConsoleAndLog("Basic Level");
 				   result1 = levelLoop(3);
-				   toConsoleAndLog("Basic Level done.");	   
+				   outputLogger.toConsoleAndLog("Basic Level done.");	   
 			   }
 			   else if (result2.equals(""))
 			   {
 				   //call second level
-				   toConsoleAndLog("Intermediate Level");
+				   outputLogger.toConsoleAndLog("Intermediate Level");
 				   result2 = levelLoop(5);
-				   toConsoleAndLog("Intermediate Level done.");
+				   outputLogger.toConsoleAndLog("Intermediate Level done.");
 			   }
 			   else if (result3.equals(""))
 			   {
 				   //call third level
-				   toConsoleAndLog("Advanced Level");
-
+				   outputLogger.toConsoleAndLog("Advanced Level");
 				   result3 =levelLoop(7);
-				   toConsoleAndLog("Advanced Level done");
+				   outputLogger.toConsoleAndLog("Advanced Level done");
 			   }
-		   }
-		   
+		   }	   
 		   while (levelScores.size() < 3) //finishes populating arrayList in event of early exit
 		   {
 			   levelScores.add(0.0);
 		   }
-	   }
-	   	   
+	   }	   	   
 	   //***************************************************************
 	   //
 	   //  Method:       levelLoop
@@ -282,7 +199,6 @@ public class Program4 {
 		   double averageScore = 0;
 		   int totalGuesses = 0;
 		   String choice = "";
-		   //String answer = "-9999"; //initial value outside bounds of possible entries
 		   int correctFirst = 0;
 		 //keeps user in loop of generate, evaluate, answer until accepted exit statement is given
 		   while (!exitStatements.contains(choice))
@@ -304,60 +220,56 @@ public class Program4 {
 			   equationEvaluator.evaluate();
 			   int result = equationEvaluator.getResult();
 		  
-			   toConsoleAndLog(generatedEquation + " = ?");
+			   outputLogger.toConsoleAndLog(generatedEquation + " = ?");
 			   while (Integer.valueOf(answer) != result)//loop continues until correct answer given
 			   {
 				   answer = input.nextLine().trim();
-				   toLog(answer);
+				   outputLogger.toLog(answer);
 				   attemptsAtQuestion++;
 				   totalGuesses++;
 			   	   
 				   while (!isValidFormat(answer)) //validation prevents possible conversion exception
 				   {
-					   toConsoleAndLog("Invalid answer format, try again");
-					   toConsoleAndLog(generatedEquation + " = ?");
+					   outputLogger.toConsoleAndLog("Invalid answer format, try again");
+					   outputLogger.toConsoleAndLog(generatedEquation + " = ?");
 					   answer = input.nextLine().trim();
-					   toLog(answer);
+					   outputLogger.toLog(answer);
 					   attemptsAtQuestion++;
 					   totalGuesses++;
 				   }
 			   
-				   if ( Integer.valueOf(answer) == result && attemptsAtQuestion == 1)
+				   if (Integer.valueOf(answer) == result && attemptsAtQuestion == 1)
 				   {
 					   correctFirst++; 	
-					   toConsoleAndLog(correctAnswer[randomResponse.nextInt(5)]);
+					   outputLogger.toConsoleAndLog(correctAnswer[randomResponse.nextInt(5)]);
 				   }
 			   
 				   else if ( Integer.valueOf(answer) == result)
 				   {
-				  	toConsoleAndLog(correctAnswer[randomResponse.nextInt(5)]);	   
+					   outputLogger.toConsoleAndLog(correctAnswer[randomResponse.nextInt(5)]);	   
 				   }
 				   else
 				   {
-					   toConsoleAndLog(wrongAnswer[randomResponse.nextInt(5)]);
-					   toConsoleAndLog(generatedEquation + " = ?");
+					   outputLogger.toConsoleAndLog(wrongAnswer[randomResponse.nextInt(5)]);
+					   outputLogger.toConsoleAndLog(generatedEquation + " = ?");
 				   }
 			   }//loop exits here when input matches result
-		   
-		   //answer = "-9999"; //resets to avoid false positive if back to back questions have same answer
-		   	   
-		   //conditionals to allow exit of function primary loop
+			   		//conditionals to allow exit of function primary loop
 		   		if (correctFirst >= 5 && size < MAX_LEVEL_SIZE)
 		   		{
-		   			toConsoleAndLog("\nEnter 'next' to move onto the next difficulty level");
-		   			toConsoleAndLog("Enter 'exit' to terminate the program");
-		   			toConsoleAndLog("Any other input will continue at current difficulty.");
-		   			choice = input.nextLine();
-		   			toLog(choice);
+		   			outputLogger.toConsoleAndLog("\nEnter 'next' to move onto the next difficulty level");
+		   			outputLogger.toConsoleAndLog("Enter 'exit' to terminate the program");
+		   			outputLogger.toConsoleAndLog("Any other input will continue at current difficulty.");
+		   			choice = input.nextLine().toLowerCase();
+		   			outputLogger.toLog(choice);
 		   		}
 		   		else if(correctFirst >= 5)
 		   		{
-		   			toConsoleAndLog("\nEnter 'exit' to terminate program.");
-		   			toConsoleAndLog("Any other input will continue at current difficulty.");
-		   			choice = input.nextLine();
-		   			toLog(choice);
-		   		}
-		   
+		   			outputLogger.toConsoleAndLog("\nEnter 'exit' to terminate program.");
+		   			outputLogger.toConsoleAndLog("Any other input will continue at current difficulty.");
+		   			choice = input.nextLine().toLowerCase();
+		   			outputLogger.toLog(choice);
+		   		}		   
 		   }
 		   //should not be possible, but prevents possible divide by 0 exception
 		   if (totalGuesses != 0) 
@@ -385,5 +297,4 @@ public class Program4 {
 	       System.out.println("Program:  4");
 	       System.out.println("Due Date: 04/28/2025\n");
 	    } // End of the developerInfo method
-	   
-}
+}//End of program4 class
